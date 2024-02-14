5e6a3cd06241377d675edcb4d39e7b194980d8b3
floomoo <elvinthomasusa@gmail.com>
2024-02-14 13:52:06 -0500

    nothing on page for adding componant file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavLink } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavLink href='#home'>home</NavLink>
     </div>
   );
 }
