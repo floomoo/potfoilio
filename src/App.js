08a45bf978fd42aaee47c16eb53659bfcfdcc3a5
floomoo <elvinthomasusa@gmail.com>
2024-02-14 14:04:30 -0500

    l

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import { NavLink } from 'react-bootstrap';
-=======
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import 'bootstrap/dist/CSS/bootstrap.min.css';
->>>>>>> e353e64a006d9a5bf9cdadd88bdcd065f5f991b0
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <NavLink href='#home'>home</NavLink>
-=======
-      <NavBar />
-      <Banner />
->>>>>>> e353e64a006d9a5bf9cdadd88bdcd065f5f991b0
     </div>
   );
 }
